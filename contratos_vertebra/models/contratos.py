71618c7e045953ff5d230bb942eb2228eb7971c2
Carlos Villarreal <proyectos@daten.com.co>
2021-12-23 12:05:52 -0500

    [ADD] Campos para modelo Contratos

--- contratos_vertebra/models/contratos.py
+++ contratos_vertebra/models/contratos.py
@@ -9,7 +9,28 @@ class Contratos_Principal(models.Model):
     objeto = fields.Text(string = 'Objeto')
     fecha_inicio =fields.Date(string = 'Fecha de Inicio')
     fecha_final = fields.Date(string='Fecha Final')
-    etapas = fields.Many2one('etapas', string="Etapas")
+    etapas = fields.Many2one('etapas', string='Etapas')
+    cliente = fields.Many2one('res.partner', string='Cliente')
+    responsable = fields.Many2one('res.users',string='Responsable')
+    valor_inicial =fields.Monetary(string='Valor Inicial', currency_field='moneda')
+    valor_total = fields.Monetary(string='Valor Total', currency_field='moneda')
+    valor_facturado = fields.Monetary(string='Valor Facturado', currency_field='moneda')
+    gastos = fields.Monetary(string='Gastos', currency_field='moneda')
+
+    def _default_currency(self):
+        return self.env['res.currency'].search([('name', '=', 'COP')], limit=1).id
+
+    moneda = fields.Many2one('res.currency', string='Moneda', default=_default_currency)
+    alcance = fields.Html(string='Alcance')
+    pct_facturado = fields.Float(string='Pct Facturado')
+    contrato = fields.Boolean(string='Contrato')
+    polizas = fields.Boolean(string='Pólizas')
+    oferta_tecnica = fields.Boolean(string='Oferta Técnica')
+    anexos = fields.Boolean(string='Anexos al Contrato')
+    matriz = fields.Boolean(string='Matriz Contractual')
+    presupuesto = fields.Boolean(string='Presupuesto General Inicial')
+    informe = fields.Boolean(string='Informe de Materializacaión')
+
 
 class Etapas (models.Model):
     _name = 'etapas'
